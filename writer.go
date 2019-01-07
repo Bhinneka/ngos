c08d2c5ce5a4ecba6f8365f45d825495c8559272
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-07 17:39:54 +0700

    add csv writer

---
+++ writer.go
@@ -0,0 +1,36 @@
+package ngos
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+// Writer interface
+type Writer interface {
+	Write(datas [][]string, output string) error
+}
+
+// CSVWriter struct
+type CSVWriter struct {
+}
+
+// Write function
+func (CSVWriter) Write(datas [][]string, output string) error {
+	file, err := os.Create(output)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, value := range datas {
+		err = writer.Write(value)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
