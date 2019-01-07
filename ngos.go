f0af1a3f760fb6ed5245d97d27f7ad94b2a989c8
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-07 17:53:37 +0700

    finishing Run method

--- ngos.go
+++ ngos.go
@@ -1,24 +1,74 @@
 package ngos
 
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
 // Ngos struct
 type Ngos struct {
 	Reader Reader
 	Writer Writer
+	Args   *Arguments
 }
 
 // New function return *Ngos
-func New() *Ngos {
+func New(args *Arguments) *Ngos {
 	csvReader := CSVReader{}
 	csvWriter := CSVWriter{}
 	return &Ngos{
 		Reader: csvReader,
 		Writer: csvWriter,
+		Args:   args,
 	}
 }
 
 // Run function, will Run Ngos
 func (n *Ngos) Run() {
+	oldFile, err := os.Open(n.Args.OldCSVFile)
 
+	if err != nil {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	readerOldCSVFile := csv.NewReader(bufio.NewReader(oldFile))
+
+	// read new csv file
+	newFile, err := os.Open(n.Args.NewCSVFile)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	readerNewCSVFile := csv.NewReader(bufio.NewReader(newFile))
+
+	linesOld, err := n.Reader.Read(readerOldCSVFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	linesNew, err := n.Reader.Read(readerNewCSVFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	linesOut := n.compare(linesNew, linesOld)
+
+	fmt.Println(linesOut)
+
+	err = n.Writer.Write(linesOut, n.Args.OutputCSVFile)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func (n *Ngos) compare(a, b []string) [][]string {
